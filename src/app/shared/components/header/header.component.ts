215b31c52a5173282b8326dabb0500aca5d4b855
Rolando <rolando.corral.js@gmail.com>
2025-08-09 21:22:56 -0300

    añade header y varibles en scss

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  public isLoggedIn: boolean = true;
+
+  login() {
+    this.isLoggedIn = true;
+    console.log('login');
+  }
+  
+  public logout() {
+    this.isLoggedIn = false;
+    console.log('logout');
+    confirm('Are you sure you want to logout?');
+  }
 }
